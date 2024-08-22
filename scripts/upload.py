8c46f48a8eeac91a02c9d18cec0547153d73d5c7
Dongyu Yan <965669036@qq.com>
2024-08-22 23:59:28 +0800

    Update upload.py

--- scripts/upload.py
+++ scripts/upload.py
@@ -4,19 +4,19 @@ import os
 images_dir = 'images'
 
 # 获取目录中的所有文件名
-file_names = os.listdir(images_dir)
+# file_names = os.listdir(images_dir)
 
-# 创建 HTML 文件并写入文件名
-with open('docs/index.html', 'w') as f:
-    f.write('<html><body>\n')
-    f.write('<h1>Image Files</h1>\n')
-    f.write('<ul>\n')
+# # 创建 HTML 文件并写入文件名
+# with open('docs/index.html', 'w') as f:
+#     f.write('<html><body>\n')
+#     f.write('<h1>Image Files</h1>\n')
+#     f.write('<ul>\n')
     
-    # 将每个文件名写入 HTML 列表
-    for file_name in file_names:
-        f.write(f'<li>{file_name}</li>\n')
+#     # 将每个文件名写入 HTML 列表
+#     for file_name in file_names:
+#         f.write(f'<li>{file_name}</li>\n')
     
-    f.write('</ul>\n')
-    f.write('</body></html>\n')
+#     f.write('</ul>\n')
+#     f.write('</body></html>\n')
 
 print("HTML file 'docs/index.html' created successfully.")
